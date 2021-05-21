c2422cf236c022810bb72c924697dab4fb89a144
Anurag Roy <anuragr9847@gmail.com>
2021-05-21 19:37:51 +0530

    Add battery and network traffic modules

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",		"sb-nettraf",	1,		4},
+	{"",		"sb-battery",	1,		3},
 	{"",		"sb-memory",	1,		2},
 	{"",		"sb-clock",		60,		1},
 };
