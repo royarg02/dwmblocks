8266d358701f33ddb31b4e191b2345ea65bdc18a
Anurag Roy <anuragr9847@gmail.com>
2021-08-10 13:01:56 +0530

    [blocks.def.h] Formatting changes

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",		"sb-nettraf",	1,		4},
-	{"",		"sb-battery",	1,		3},
-	{"",		"sb-memory",	1,		2},
-	{"",		"sb-clock",		60,		1},
+  {"",      "sb-nettraf",                  1,               6},
+  {"",      "sb-battery",                  1,               5},
+  {"",      "sb-memory",                   1,               2},
+  {"",      "sb-clock",                   60,               1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
