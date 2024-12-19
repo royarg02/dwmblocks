9f2b655264ee91171000cc939ba18a002d3d42af
Anurag Roy <anuragr9847@gmail.com>
2024-12-19 20:42:06 +0530

    [blocks] Split brightness control from battery module
    - Run battery module less often
    - Trigger refresh on adapter plug in through acpi
    - Brightness module only triggerable on signal

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   {"",      "sb-nettraf",                  1,               6},
-  {"",      "sb-battery",                  1,               5},
+  {"",      "sb-brightness",               0,               5},
+  {"",      "sb-battery",                 30,               8},
   {"",      "sb-audio-sink",               0,               4},
   {"",      "sb-audio-source",             0,               3},
   {"",      "sb-memory",                   1,               2},
